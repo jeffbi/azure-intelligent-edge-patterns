8e49b7128bcb9b6d77b10e9ed122d02b2aa174bc
Jeff Bienstadt <jeffbi@jetstreamsoftware.com>
2019-07-10 17:12:43 -0700

    Allow optional upload to storage rather than redis db

--- edge-ai-void-detection/modules/processimages/main.py
+++ edge-ai-void-detection/modules/processimages/main.py
@@ -10,6 +10,7 @@ import statistics
 import threading
 import ssdvgg_utils
 from client import PredictionClient
+from blob import BlobUploader
 import pprint
 import grpc
 import cv2
@@ -31,7 +32,7 @@ import io
 from io import BytesIO
 import numpy as np
 # import visualization_utils as vis_util
-import datetime
+from datetime import datetime
 
 # messageTimeout - the maximum time in milliseconds until a message times out.
 # The timeout period starts at IoTHubModuleClient.send_event_async.
@@ -63,6 +64,9 @@ tensor_outputs = [
 ]
 
 client = PredictionClient(os.getenv('ML_MODULE_NAME','fpga-stock-module'), 50051)
+blob_uploader = None
+data_destination = "redis"
+camera_id = ""
 
 # Callback received when the message that we're forwarding is processed.
 def send_confirmation_callback(message, result, user_context):
@@ -94,10 +98,41 @@ def receive_message_callback(message, hubManager):
     return IoTHubMessageDispositionResult.ACCEPTED
 
 def module_twin_callback(update_state, payload, user_context):
-    print("inside module twin callbak")
+    print("inside module twin callback")
     print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
     print(payload)
-    print("before parseing module twin")
+    print("before parsing module twin")
+    twin = json.loads(payload)
+    parse_module_twin(twin)
+
+def parse_module_twin(twin):
+    print("Parsing module twin {}".format(twin))
+    global blob_uploader, data_destination, camera_id
+    blob_connection_string = get_twin_property(twin, "blobStorageSasUrl")
+    if blob_connection_string is not None:
+        blob_uploader = BlobUploader("still-images", blob_connection_string)
+        print("Created new BlobUploader from {}".format(blob_connection_string))
+    dest = get_twin_property(twin, "dataDestination")
+    if dest is not None:
+        data_destination = dest
+        print("Set data_destination to {}".format(data_destination))
+    id = get_twin_property(twin, "cameraId")
+    if id is not None:
+        camera_id = id
+        print("Set camera_id to {}".format(camera_id))
+
+def make_storage_filename(time=None):
+    if time is None:
+        time = datetime.utcnow()
+    return datetime.utcnow().isoformat(time="milliseconds") + 'Z'
+
+def send_iot_hub_message(hub_manager, message_dict, schema):
+    message_dict["CreationTimeUtc"] = datetime.utcnow().isoformat(timespec="seconds") + 'Z'
+    message_dict["MessageSchema"] = schema
+    message = json.dumps(message_dict)
+    message = IoTHubMessage(message)
+    hub_manager.client.send_event_async("output1", message, send_confirmation_callback, 0)
+
 
 #AREA For DETECTION
 
@@ -127,16 +162,55 @@ def score_single_image(hub_manager, file_path):
             processed_results["classes"] = classes.tolist()
             processed_results["scores"] = scores.tolist()
             processed_results["bboxes"] = bboxes.tolist()
-            messages = {}
-            messages['filename'] = file_path
-            messages['response1'] = processed_results
-            messages = json.dumps(messages)
-            message=IoTHubMessage(messages)
-            t2 = time.time()
-            hub_manager.client.send_event_async("output1", message, send_confirmation_callback, 0)
+            if data_destination == "redis":
+                messages = {}
+                messages['filename'] = file_path
+                messages['response1'] = processed_results
+                messages = json.dumps(messages)
+                message=IoTHubMessage(messages)
+                t2 = time.time()
+                hub_manager.client.send_event_async("output1", message, send_confirmation_callback, 0)
+            elif data_destination == "iothub":
+                pass #TODO: build up and send result messages to the hub, one message per void recognized
+                print("Building IoT Hub messages")
+                print(processed_results)
+                camera_time = datetime.utcnow() # time the image was "taken"
+                for i in range(len(processed_results["classes"])):
+                    message = {}
+                    message["cameraId"] = camera_id
+                    message["time"] = camera_time.isoformat(timespec="seconds") + 'Z'
+                    message["cls"] = processed_results["classes"][i]
+                    message["score"] = processed_results["scores"][i]
+                    message["bbymin"] = processed_results["bboxes"][i][0]
+                    message["bbxmin"] = processed_results["bboxes"][i][1]
+                    message["bbymax"] = processed_results["bboxes"][i][2]
+                    message["bbxmax"] = processed_results["bboxes"][i][3]
+                    send_iot_hub_message(hub_manager, message, "recognition:v1")
+                # upload original image to storage
+                if camera_id:
+                    print("Reading original image")
+                    with open(file_path, "rb") as file:
+                        img = file.read()
+                    name = make_storage_filename(camera_time)
+                    ext = os.path.splitext(file_path)[1]
+                    print("Uploading image to storage: camera id: {}, name: {}, extension: {}".format(camera_id, name, ext))
+                    blob_uploader.upload(camera_id, name, ext, img)
+                    print("Upload complete")
+                    print("Building upload message")
+                    message = {}
+                    message["cameraId"] = camera_id
+                    message["time"] = camera_time.isoformat(timespec="seconds") + 'Z'
+                    message["type"] = ext
+                    message["procType"] = "FPGA"
+                    message["procMsec"] = (time.time() - start) * 1000.0
+                    print("Sending upload message")
+                    send_iot_hub_message(hub_manager, message, "image-upload:v1")
+                t2 = time.time()
+
             print("<<Finished processing at : " + str(time.time()) + " took " + str(t2-start))
         except:
             print("Unexpected Error:", traceback.print_exc())
+
 
 def format_image_url(root, container, name):
     if(root.endswith("/")):
